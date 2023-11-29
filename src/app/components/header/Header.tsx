41316af685e74753f90a4094b51d8a22d4cfbbe5
Yogesh Kumar <yogeshkumar@CORPLC9151525.local>
2023-11-29 14:45:49 +0530

    Sticky Header

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,11 +1,26 @@
+"use client";
 import Image from 'next/image';
 import './header.css';
 import LandingPage from '../../assets/LandingPage.png';
 import Title from "../../assets/Title.jpeg";
+import { useEffect, useState } from 'react';
 
 const Header = () => {
+
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 70) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    });
+  }, []);
+
   return (
-    <div className='landing_page__header'>
+    <div className='landing_page__header' style={{position: isSticky ? 'sticky' : 'static', top: isSticky ? -100 : null, zIndex: isSticky?1:null}}>
       <div className='landing_page__header_title'>
         <div className='landing_page__header_title_text'>
           <p className='text-white text-[2rem]'>Domino's online ordering</p>
